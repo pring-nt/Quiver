ac8e8e4f9a77369ad77263484bf6d77524a28f8c
nathan <natetrndd@gmail.com>
2026-04-19 00:06:41 +0800

    feat: create barrel file for Announcement components

---
+++ src/lib/components/layout/dialogs/index.ts
@@ -0,0 +1,3 @@
+export {default as BaseDialog} from './BaseDialog.svelte';
+export {default as AnnouncementsDialog} from './announcements/AnnouncementsDialog.svelte';
+export {default as AnnouncementCard} from './announcements/AnnouncementCard.svelte';
